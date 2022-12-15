bba8a54d06e6dad6354cf980fca64e3068108d4b
unknown <datnh@ehiway.co>
2022-12-15 16:33:01 +0700

    for update

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,8 @@
 <?php
+/** Enable W3 Total Cache */
+define('WP_CACHE', true); // Added by W3 Total Cache
+
+
 /**
  * The base configuration for WordPress
  *
@@ -18,85 +22,72 @@
  * @package WordPress
  */
 
-// ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
+//Using environment variables for memory limits
+$wp_memory_limit = (getenv('WP_MEMORY_LIMIT') && preg_match("/^[0-9]+M$/", getenv('WP_MEMORY_LIMIT'))) ? getenv('WP_MEMORY_LIMIT') : '128M';
+$wp_max_memory_limit = (getenv('WP_MAX_MEMORY_LIMIT') && preg_match("/^[0-9]+M$/", getenv('WP_MAX_MEMORY_LIMIT'))) ? getenv('WP_MAX_MEMORY_LIMIT') : '256M';
+
+/** General WordPress memory limit for PHP scripts*/
+define('WP_MEMORY_LIMIT', $wp_memory_limit );
+
+/** WordPress memory limit for Admin panel scripts */
+define('WP_MAX_MEMORY_LIMIT', $wp_max_memory_limit );
+
+
 //Using environment variables for DB connection information
 
-
-
-// ** MySQL settings - You can get this info from your web host ** //
-
-/** The name of the database for WordPress */
-
-
-
+// ** Database settings - You can get this info from your web host ** //
 $connectstr_dbhost = getenv('DATABASE_HOST');
-
 $connectstr_dbname = getenv('DATABASE_NAME');
-
 $connectstr_dbusername = getenv('DATABASE_USERNAME');
-
 $connectstr_dbpassword = getenv('DATABASE_PASSWORD');
 
-
-
-
-define('DATABASE_NAME', $connectstr_dbname);
-
-
+/** The name of the database for WordPress */
+define('DB_NAME', $connectstr_dbname);
 
 /** MySQL database username */
-
-define('DATABASE_USERNAME', $connectstr_dbusername);
-
-
+define('DB_USER', $connectstr_dbusername);
 
 /** MySQL database password */
-
-define('DATABASE_PASSWORD',$connectstr_dbpassword);
-
-
+define('DB_PASSWORD',$connectstr_dbpassword);
 
 /** MySQL hostname */
-
-define('DATABASE_HOST', $connectstr_dbhost);
-
-
+define('DB_HOST', $connectstr_dbhost);
 
 /** Database Charset to use in creating database tables. */
-
 define( 'DB_CHARSET', 'utf8' );
 
-
-
 /** The Database Collate type. Don't change this if in doubt. */
-
 define( 'DB_COLLATE', '' );
 
+/** Enabling support for connecting external MYSQL over SSL*/
+$mysql_sslconnect = (getenv('DB_SSL_CONNECTION')) ? getenv('DB_SSL_CONNECTION') : 'true';
+if (strtolower($mysql_sslconnect) != 'false' && !is_numeric(strpos($connectstr_dbhost, "127.0.0.1")) && !is_numeric(strpos(strtolower($connectstr_dbhost), "localhost"))) {
+	define('MYSQL_CLIENT_FLAGS', MYSQLI_CLIENT_SSL);
+}
+
+
 /**#@+
- * Authentication unique keys and salts.
+ * Authentication Unique Keys and Salts.
  *
- * Change these to different unique phrases! You can generate these using
- * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
- *
- * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'i-}Q_s@RA56*Dox$VRwuS>w+,@R]!O18i8QJ#5{ Buv1p$K~|X?1|ZT(*sKay/%R' );
-define( 'SECURE_AUTH_KEY',  'q4YB[Wk7C]DVce7x8%WSmwxk}CG4C7Jbyrrf^K`QQo66Cu=VJU|MBOASk4[a7z!w' );
-define( 'LOGGED_IN_KEY',    '}F<3[Y)XeZ0XGS3d8pM21I:]2u?u+HmuEI<vV7-aO>akelt3n<yeKo0AB[pWh2[q' );
-define( 'NONCE_KEY',        'L[_QBm@s/:H.Z[mww~5C=_.&B(p?Cm9UcVF8-#&^J U1bcQ@YTYy|dp9T{niIC44' );
-define( 'AUTH_SALT',        '>yY2-HH]?x/BR4:zwMerR C)`fKv+NbL^fA-ju=tUKBjW!Sq>n$A+EJukL>z;;_-' );
-define( 'SECURE_AUTH_SALT', '5=)6S40%mb^e]xFtDU(}G2~`7{PECy4B*{E)0Z0,Xof0k M^%^]ZRzpC{wk:s##G' );
-define( 'LOGGED_IN_SALT',   'd,DIS:RZ>XL#m%HM|t:~sYC=-M$N9}EC3>=Z6<2!W>aSJH5S9!.2`JybSz;Hl%tL' );
-define( 'NONCE_SALT',       'wJ dQ@{;GGnOe?j=$W5RFY]=u&9>I6,tui(1a)Bru7M$^ntfJj12IBhLeHJ|<2(}' );
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
 
 /**#@-*/
 
 /**
- * WordPress database table prefix.
+ * WordPress Database Table prefix.
  *
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
@@ -117,11 +108,21 @@ $table_prefix = 'wp_';
  */
 define( 'WP_DEBUG', false );
 
-/* Add any custom values between this line and the "stop editing" line. */
+/* That's all, stop editing! Happy blogging. */
+/**https://developer.wordpress.org/reference/functions/is_ssl/ */
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+	$_SERVER['HTTPS'] = 'on';
 
+$http_protocol='http://';
+if (!preg_match("/^localhost(:[0-9])*/", $_SERVER['HTTP_HOST']) && !preg_match("/^127\.0\.0\.1(:[0-9])*/", $_SERVER['HTTP_HOST'])) {
+	$http_protocol='https://';
+}
 
-
-/* That's all, stop editing! Happy publishing. */
+//Relative URLs for swapping across app service deployment slots
+define('WP_HOME', $http_protocol . $_SERVER['HTTP_HOST']);
+define('WP_SITEURL', $http_protocol . $_SERVER['HTTP_HOST']);
+define('WP_CONTENT_URL', '/wp-content');
+define('DOMAIN_CURRENT_SITE', $_SERVER['HTTP_HOST']);
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
